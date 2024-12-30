783175109c3bbfe1da12b7ed3b20638434fd2ac5
tesseract <146037313+DoTheBestToGetTheBest@users.noreply.github.com>
2024-12-30 07:10:56 -0800

    Update block.rs

--- src/block.rs
+++ src/block.rs
@@ -15,36 +15,38 @@ impl<B: AlloyBlock> From<B> for RevmAlloyBlock<B> {
 impl<B> RevmBlock for RevmAlloyBlock<B>
 where
     B: AlloyBlock,
-{
+{    
+    #[inline]
     fn number(&self) -> u64 {
         self.0.number()
     }
-
+    #[inline]
     fn beneficiary(&self) -> Address {
         self.0.beneficiary()
     }
-
+    #[inline]
     fn timestamp(&self) -> u64 {
         self.0.timestamp()
     }
-
+    
     #[doc = " The gas limit of the block."]
+    #[inline]
     fn gas_limit(&self) -> u64 {
         self.0.gas_limit()
     }
-
+    #[inline]
     fn basefee(&self) -> u64 {
         self.0.base_fee_per_gas().unwrap_or(u64::MAX)
     }
-
+    #[inline]
     fn difficulty(&self) -> U256 {
         self.0.difficulty()
     }
-
+    #[inline]
     fn prevrandao(&self) -> Option<B256> {
         self.0.parent_beacon_block_root()
     }
-
+    #[inline]
     fn blob_excess_gas_and_price(&self) -> Option<BlobExcessGasAndPrice> {
         self.0.excess_blob_gas().map(BlobExcessGasAndPrice::new)
     }
